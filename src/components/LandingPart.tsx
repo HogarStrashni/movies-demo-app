59021b3b727654c4abf563aaf73d8a497b778379
Đorđe Matić <djmatic@agfbl.org>
2022-10-10 21:41:17 +0200

    fix bug - implement responsive LandingPart

--- src/components/LandingPart.tsx
+++ src/components/LandingPart.tsx
@@ -1,12 +1,14 @@
 const LandingPart = () => {
   return (
     <div className="h-[calc(100%-120px)] py-auto text-gray-300 text-center flex flex-col justify-center flex-1">
-      <h1 className="text-[2.5rem] pb-8 text-blue-400">
+      <h1 className="px-4 text-[1.75rem] pb-8 text-blue-400 sm:text-[2.5rem]">
         ADD MOVIE TO YOUR LIST
       </h1>
-      <p className="text-2xl">Find all about movies with a simple search</p>
+      <p className="px-4 sm:text-2xl">
+        Find all about movies with a simple search
+      </p>
       <p>or</p>
-      <p className="text-2xl">
+      <p className="px-4 sm:text-2xl">
         Make your plan which movies to watch next weekend
       </p>
     </div>
